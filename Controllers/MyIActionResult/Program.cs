7994792becf912763bf1defbb2180654a35f7e3c
Edmond Guo <edmond.guo@edg.com.au>
2025-09-20 16:21:40 +1000

    Add Model Binding PDF and comment out XML serializer formatter in Program.cs

--- Controllers/MyIActionResult/Program.cs
+++ Controllers/MyIActionResult/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddControllers().AddXmlSerializerFormatters();
 builder.Services.AddControllers();
-
 var app = builder.Build();
 
 app.UseStaticFiles();
