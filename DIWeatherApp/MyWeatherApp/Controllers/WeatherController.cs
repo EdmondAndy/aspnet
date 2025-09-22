c7a3a5d9f500c9e0b49fdcd4eb933c8fe97df1f8
Edmond Guo <edmond.guo@edg.com.au>
2025-09-22 10:55:20 +1000

    Implement Weather feature with CityWeather model, WeatherService, and WeatherController; add views and styling

---
+++ DIWeatherApp/MyWeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using DIWeatherApp.Models;
+using ServiceContracts;
+using Services;
+
+namespace MyWeatherApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        //private field to hold reference to the service instance
+        private readonly IWeatherService _weatherService;
+        // Create a constructor and inject IWeatherService
+        public WeatherController(IWeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        //When a HTTP GET request is received at path "/"
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //invoke service method
+            var cities = _weatherService.GetWeatherDetails();
+            //send cities collection to "Views/Weather/Index" view
+            return View(cities);
+        }
+
+        [Route("/weather/{cityCode}")]
+        public IActionResult Weather(string? cityCode)
+        {
+            //if cityCode is not supplied in the route parameter
+            if (string.IsNullOrEmpty(cityCode))
+            {
+                return View();
+            }
+
+            //invoke service method (get matching city object based on the city code)
+            CityWeather? city = _weatherService.GetWeatherByCityCode(cityCode);
+
+            //send matching city object to "Views/Weather/Index" view
+            return View(city);
+        }
+    }
+}
